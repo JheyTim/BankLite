12024d83ac0324284de2c7be1f5814e49711ac31
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2026-05-17 22:45:25 +0800

    chore(messaging): add accound events queue

--- libs/messaging/src/rabbitmq/rabbitmq.constants.ts
+++ libs/messaging/src/rabbitmq/rabbitmq.constants.ts
@@ -1,14 +1,19 @@
 /**
  * Injection token for the BankLite RabbitMQ client.
  */
-export const BANKLITE_RABBITMQ_CLIENT = 'BANKLITE_RABBITMQ_CLIENT';
+export const BANKLITE_RABBITMQ_CLIENT = "BANKLITE_RABBITMQ_CLIENT";
 
 /**
  * Queue used by User Profile Service.
  */
-export const PROFILE_EVENTS_QUEUE = 'profile.events';
+export const PROFILE_EVENTS_QUEUE = "profile.events";
 
 /**
  * Queue used by KYC Service.
  */
-export const KYC_EVENTS_QUEUE = 'kyc.events';
+export const KYC_EVENTS_QUEUE = "kyc.events";
+
+/**
+ * Queue used by Account Service.
+ */
+export const ACCOUNT_EVENTS_QUEUE = "account.events";
