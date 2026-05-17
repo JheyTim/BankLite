1418e61d04e8d9899b4b7e8b343117ec823d57aa
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2026-05-17 22:55:08 +0800

    feat(contracts): add account lifecycle events

--- libs/contracts/src/events/index.ts
+++ libs/contracts/src/events/index.ts
@@ -2,3 +2,5 @@ export * from './user-registered.event';
 export * from './file-uploaded.event';
 export * from './kyc-verified.event';
 export * from './kyc-rejected.event';
+export * from './account-created.event';
+export * from './account-activated.event';
